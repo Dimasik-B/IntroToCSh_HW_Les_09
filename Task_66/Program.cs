e3f73bf0d471bab49f3f3b1181b74747e7a25da5
Dima Bykadorov <dimka.b@live.com>
2023-09-03 01:30:56 +0300

    Add Task 66

---
+++ Task_66/Program.cs
@@ -0,0 +1,12 @@
+﻿Console.Write("Введите число: ");
+int m = int.Parse(Console.ReadLine()!);
+Console.Write("Введите число: ");
+int n = int.Parse(Console.ReadLine()!);
+Console.WriteLine($"Cумма натуральных элементов в промежутке от {m} до {n} равна {NaturalNumbersSum(m, n)}");
+
+int NaturalNumbersSum(int firstNumber, int lastNmber)
+{
+    if (firstNumber == lastNmber) return lastNmber;
+    if (firstNumber < lastNmber) return (firstNumber += NaturalNumbersSum(firstNumber + 1, lastNmber));
+    return (lastNmber += NaturalNumbersSum(firstNumber, lastNmber + 1));
+}
