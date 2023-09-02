c4c509f3adfe80bf7cadcbe940ace74827d81565
Dima Bykadorov <dimka.b@live.com>
2023-09-03 01:28:46 +0300

    Add task 64

---
+++ Task_64/Program.cs
@@ -0,0 +1,9 @@
+﻿Console.WriteLine("Введите число: ");
+int number = int.Parse(Console.ReadLine()!);
+Console.WriteLine(NaturalNumbersBackwards(1, number));
+
+string NaturalNumbersBackwards(int firstNumber, int lastNmber)
+{
+    if (lastNmber == firstNumber) return firstNumber.ToString();
+    return (lastNmber + ", " + NaturalNumbersBackwards(firstNumber, lastNmber - firstNumber).ToString());
+}
