2592ca1e27c300fdc67bad6f06d8a82c3313f546
Dima Bykadorov <dimka.b@live.com>
2023-09-03 01:36:07 +0300

    Add Task 68

---
+++ Task_68/Program.cs
@@ -0,0 +1,12 @@
+﻿Console.WriteLine("Введите число m: ");
+int m = int.Parse(Console.ReadLine()!);
+Console.WriteLine("Введите число n: ");
+int n = int.Parse(Console.ReadLine()!);
+Console.WriteLine($"{Akkerman(m, n)}");
+
+int Akkerman(int m, int n)
+{
+    if (m == 0) return n + 1;
+    else if (m > 0 && n == 0) return Akkerman(m - 1, 1);
+    return Akkerman(m - 1, Akkerman(m, n - 1));
+}
